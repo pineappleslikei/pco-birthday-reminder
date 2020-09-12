4c13e374fdb4f4a9fbdcd8ceab06b325923eb2d7
Chris Ellis <ellis.h.chris@gmail.com>
2020-09-11 20:59:53 -0700

    Initial Commit

---
+++ constants.py
@@ -0,0 +1,4 @@
+from datetime import datetime
+
+today = datetime.today().strftime('%m-%d')
+today2 = datetime.today().strftime('%m-%d-%Y')
---
+++ yoursecrets.py
@@ -0,0 +1,8 @@
+
+# log into planning center developers and generate these values
+pco_app_id = ''
+pco_key = ''
+
+# if using gmail to send email, go to "google->account->security->signing into google->app passwords" and generate this value
+sender_email = ''
+app_pw = ''
