fd34fdeb5eded27c353647ac503bc5af7de8b08f
Chris Ellis <ellis.h.chris@gmail.com>
2020-09-13 20:22:36 -0700

    Add get_next_plan function

--- pco.py
+++ pco.py
@@ -3,7 +3,8 @@ import pco_config as pco
 import credentials as cred
 
 base_url = 'https://api.planningcenteronline.com/services/v2/'
-tech_team = []
+tech_team = {}
+next_plans = {}
 
 
 def get_team_members(team_id):
@@ -14,5 +15,15 @@ def get_team_members(team_id):
         person_name = person['attributes']['full_name']
         # if someone hasn't set their birthday it will be saved as "None"
         person_bday = person['attributes']['birthdate']
-        tech_team.append(
+        tech_team.update(
             {person_name: {'id': person_id, 'birthday': person_bday}})
+
+
+def get_next_plan(service_id):
+    response = requests.get(
+        base_url + f'service_types/{service_id}/plans?filter=future&per_page=1', auth=(cred.pco_app_id, cred.pco_key)).json()
+    if len(response['data']) > 0:
+        next_plan_date = response['data'][0]['attributes']['sort_date']
+        next_plan_id = response['data'][0]['id']
+        next_plans.update(
+            {service_id: {'plan_id': next_plan_id, 'date': next_plan_date}})
