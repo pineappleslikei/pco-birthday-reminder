b15fcedf503b743e81eed16445952ca8b7b5c7ef
Chris Ellis <ellis.h.chris@gmail.com>
2020-09-14 21:28:31 -0700

    Add function to retrieve a user's next plan

--- pco.py
+++ pco.py
@@ -1,10 +1,23 @@
 import requests
 import pco_config as pco
 import credentials as cred
+from datetime import datetime
 
 base_url = 'https://api.planningcenteronline.com/services/v2/'
 tech_team = {}
 next_plans = {}
+weekend_team = []
+
+
+def get_user_next_plan(person_id):
+    response = requests.get(
+        base_url + f'people/{person_id}/schedules', auth=(cred.pco_app_id, cred.pco_key)).json()
+    if len(response['data']) > 0:
+        next_plan_date = datetime.strptime(
+            response['data'][0]['attributes']['sort_date'], '%Y-%m-%dT%H:%M:%S%z').replace(tzinfo=None)
+        return next_plan_date
+    else:
+        return None
 
 
 def get_team_members(team_id):
@@ -15,15 +28,18 @@ def get_team_members(team_id):
         person_name = person['attributes']['full_name']
         # if someone hasn't set their birthday it will be saved as "None"
         person_bday = person['attributes']['birthdate']
-        tech_team.update(
-            {person_name: {'id': person_id, 'birthday': person_bday}})
+        if person_bday != None:
+            current_bday = datetime.strptime(
+                person_bday, '%Y-%m-%d').replace(year=datetime.today().year)
+            tech_team.update(
+                {person_name: {'id': person_id, 'birthday': current_bday, 'next plan': get_user_next_plan(person_id)}})
 
 
 def get_next_plan(service_id):
     response = requests.get(
         base_url + f'service_types/{service_id}/plans?filter=future&per_page=1', auth=(cred.pco_app_id, cred.pco_key)).json()
     if len(response['data']) > 0:
-        next_plan_date = response['data'][0]['attributes']['sort_date']
+        next_plan_date = datetime.strptime(
+            response['data'][0]['attributes']['sort_date'], '%Y-%m-%dT%H:%M:%S%z').replace(tzinfo=None)
         next_plan_id = response['data'][0]['id']
-        next_plans.update(
-            {service_id: {'plan_id': next_plan_id, 'date': next_plan_date}})
+        return next_plan_date
