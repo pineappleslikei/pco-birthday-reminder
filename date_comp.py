cc74eefdb8c9aba59769ca13d682390eb714befc
Chris Ellis <ellis.h.chris@gmail.com>
2020-09-18 17:19:28 -0700

    Edit bday_priority func to parse list of dates

--- date_comp.py
+++ date_comp.py
@@ -9,18 +9,31 @@ bday_scheduled = {}
 # variable storing next "Elevate" service time
 next_elevate_plan = p.get_next_plan(pco.service_ids['Elevate'])
 
-# need to add a check for if the users 2nd next plan is before birthday too. probably better solution than timedelta
-def bday_priority(name, birthday, next_user_plan):
-    if today < birthday and birthday < next_elevate_plan:
-        bday_before_weekend.update({name: birthday})
-    if next_user_plan == next_elevate_plan:
-        if today < birthday and next_user_plan < birthday:
-            sevendays = timedelta(days=7)
-            week_plus_1 = next_user_plan + sevendays
-            if birthday < week_plus_1:
-                bday_scheduled.update({name: birthday})
+# sorts team members into 2 categories of upcoming birthdays:
+# 1) team members that have a birthday this week before the weekend/are not scheduled this weekend
+# 2) team members that are scheduled for the upcoming weekend and have a birthday before they are scheduled again
+
+
+def bday_priority(name, birthday, next_user_plans):
+    if today < birthday and birthday <= next_elevate_plan:
+        if len(next_user_plans) > 0:
+            if next_user_plans[0] != next_elevate_plan:
+                bday_before_weekend.update({name: birthday})
+        else:
+            bday_before_weekend.update({name: birthday})
+    if len(next_user_plans) > 0:
+        # checks if the team member is serving the upcoming weekend
+        if next_user_plans[0] == next_elevate_plan:
+            try:
+                if today < birthday and birthday < next_user_plans[1]:
+                    bday_scheduled.update({name: birthday})
+            except IndexError:
+                if today < birthday and next_user_plans[0] <= birthday:
+                    bday_scheduled.update({name: birthday})
 
 # builds a text string of reminders for team members who's birthdays will happen before their next scheduled plan
+
+
 def body_builder_catch(dict_of_bdays):
     bday_catch = []
     for name, bday in dict_of_bdays.items():
@@ -30,6 +43,8 @@ def body_builder_catch(dict_of_bdays):
     return bday_str
 
 # builds a text string of reminders for team members who are scheduled the next plan and will have their birthday before their next plan
+
+
 def body_builder_sched(dict_of_bdays):
     bday_catch = []
     for name, bday in dict_of_bdays.items():
